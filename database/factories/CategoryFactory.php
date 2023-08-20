2f9ce622b62b2d9e00d07e1032aa8f304c4c076b
ajagabos007 <ajagabos007@gmail.com>
2023-08-21 00:22:18 +0100

    category merge conflict

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,17 +17,10 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-<<<<<<< HEAD
-            'name'=>$this->faker->name(),
-            'slug'=>$this->faker->slug(),
-            'parent_category_id'=>$this->faker->unique(),
-            'description'=>$this->faker->text()
-=======
             'name' => fake()->unique()->word(), 
             'description' => fake()->realText(),
             'slug' => fake()->unique()->slug(),
             'parent_category_id' => null
->>>>>>> 501db72082300fc37744af6d0b1af26809249f5b
         ];
     }
 }
