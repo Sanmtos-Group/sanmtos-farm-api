9da6f27dc8ff01853c920ef3f78eb10724d2d9b6
ajagabos007 <ajagabos007@gmail.com>
2023-08-21 00:48:08 +0100

    update product migration

--- database/migrations/2023_08_14_144644_create_products_table.php
+++ database/migrations/2023_08_14_144644_create_products_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Store;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,9 +16,16 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->text('description')->nullable();
+            $table->longText('description')->nullable();
             $table->string('short_description')->nullable();
+            $table->unsignedDecimal('price', $precision = 19, $scale = 2)->default(0.01); 
+            $table->integer('discount')->default(0)->min(0)->max(100);
+            $table->foreignIdFor(Category::class)->cascadeOnUpdate()->cascadeOnDelete();            
+            $table->foreignIdFor(Store::class)->cascadeOnUpdate()->cascadeOnDelete();            
+            $table->timestamp('verified_at', $precision = 0)->nullable();
+            $table->foreignId('verifier_id')->nullable()->constrained('users', 'id')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
