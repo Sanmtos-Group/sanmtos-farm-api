2a9cbcdfe1656f3d7cba708501d92fb96e120b26
ajagabos007 <ajagabos007@gmail.com>
2023-08-24 12:43:41 +0100

    add store delete test

---
+++ tests/Feature/Store/DeleteStoreTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Store;
+
+use App\Models\User;
+Use App\Traits\Testing\WithStore;
+Use App\Traits\Testing\FastRefreshDatabase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+use Tests\TestCase;
+
+class DeleteStoreTest extends TestCase
+{
+    use FastRefreshDatabase;
+    use WithFaker;
+    use WithStore;
+
+    /**
+     * User can update a store test
+     *
+     * @return void
+     */
+    public function test_user_can_delete_store() : void
+    {
+        $user = $this->store->user;
+        $this->actingAs($user);
+
+        $response = $this->patch(route('api.stores.destroy', $this->store));
+        $this->store->refresh();
+
+        $response->assertValid();
+        $response->assertOk();
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseHas($this->store::class, $this->store->only($this->store->getFillable()));
+        $response->assertJson(fn (AssertableJson $json) =>$json->etc());
+    }
+
+
+    /**
+     * Setup store test environment.
+     * 
+     * @override Illuminate\Foundation\Testing\TestCase  setUp()
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpStore();
+    }
+}
