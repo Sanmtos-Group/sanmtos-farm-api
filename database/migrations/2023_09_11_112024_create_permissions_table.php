85be7f6b7ee4a130f03d5f7f01c7a3d6f9484e8d
Ogundipe Olusegun <emma1994204@gmail.com>
2023-09-11 06:18:48 -0700

    Roles and Permission added

--- database/migrations/2023_09_11_112024_create_permissions_table.php
+++ database/migrations/2023_09_11_112024_create_permissions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('permissions', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->boolean('is_assignable')->default('false');
             $table->timestamps();
