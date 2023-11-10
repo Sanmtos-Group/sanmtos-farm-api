e35707110fa43d3724c6fefb4b77cc18289d604b
Ogundipe Olusegun <emma1994204@gmail.com>
2023-11-10 01:03:03 -0800

    Coupon Police done

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CouponResource;
 use App\Models\Coupon;
 use App\Http\Requests\StoreCouponRequest;
 use App\Http\Requests\UpdateCouponRequest;
+use App\Models\Product;
 
 class CouponController extends Controller
 {
+    public function __construct(){
+        $this->authorizeResource(Coupon::class, 'coupon');
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $promos = Coupon::all();
+        $promo_resource = new CouponResource($promos);
+        return $promo_resource;
     }
 
     /**
@@ -29,7 +37,16 @@ class CouponController extends Controller
      */
     public function store(StoreCouponRequest $request)
     {
-        //
+        $promo_data = $request->validated();
+
+        $query = Product::where('id', $promo_data->id);
+
+        $promo = Coupon::create($promo_data);
+
+        $promo_resource = new CouponResource($promo);
+        $promo_resource->with['message'] = 'Coupon created successfully';
+
+        return $promo_resource;
     }
 
     /**
--- app/Policies/CouponPolicy.php
+++ app/Policies/CouponPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\Coupon;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 use App\Traits\Policy\AuthorizeAllActionToSuperAdmin;
@@ -49,9 +50,29 @@ class CouponPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Coupon $coupon): bool
+    public function delete(User $user, Coupon $coupon, Role $role): bool
     {
-        return Auth::user() == $user->owns_a_store || $user->owns_a_store || $user->hasPermission('delete coupon') || true;
+        $can_delete_coupon = $user->hasPermission('delete coupon');
+
+        // store owner can only delete store coupon
+        if($user->owns_a_store){
+            $can_delete_coupon = $can_delete_coupon && $user->store->id == $coupon->store_id;
+        }
+        // sanmtos staff can only delete non store coupon
+        elseif($user->is_staff){
+            $can_delete_coupon = $can_delete_coupon && is_null($coupon->store_id);
+        }
+        // store staff can only delete store roles
+        else {
+            $can_delete_coupon = $can_delete_coupon && $user->workStores()->where('store_id', $coupon->store_id)->count();
+
+            if($can_delete_coupon){
+                $can_delete_coupon = $can_delete_coupon && $this->permissionIsGrantedByTheStoreInActionThroughRole($user, $role, 'delete coupon');
+            }
+
+        }
+
+        return $can_delete_coupon;
     }
 
     /**
