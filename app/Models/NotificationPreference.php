e4bc0fbdfe8fc773d7576fcbff67edf0aebc48a1
Philip James Ajagabos <ajagabos007@gmail.com>
2024-03-25 15:13:48 +0100

    add user subcription state to notification preference

--- app/Models/NotificationPreference.php
+++ app/Models/NotificationPreference.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-
 class NotificationPreference extends Model
 {
     use HasFactory;
@@ -23,12 +23,33 @@ class NotificationPreference extends Model
         'type',
     ];
 
-      /**
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'user_is_a_subcriber',
+    ];
+    
+
+    /**
      * The notifications preferences that belongs to the user (vendor).
      */
     public function subscribers()
     {
         return $this->belongsToMany(User::class)->using(NotificationPreferenceUser::class);
 
+    }
+
+    /**
+     * Determine if current auth user is a subscriber
+     */
+    protected function userIsASubcriber(): Attribute
+    {
+        $user = auth()->user();
+        return Attribute::make(
+            get: fn () => is_null($user)? false : !is_null($this->subscribers()->where('user_id', $user->id)->first())
+        );
     }
 }
