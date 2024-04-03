a1d40ad6462a7879104fae05020f6ab648a3ff59
Philip James Ajagabos <ajagabos007@gmail.com>
2024-04-03 06:45:25 +0100

    rm max_usage & unlimited from coupon serialization

--- app/Http/Requests/StoreCouponRequest.php
+++ app/Http/Requests/StoreCouponRequest.php
@@ -31,8 +31,8 @@ class StoreCouponRequest extends FormRequest
             'requires_min_purchase' => 'sometimes|boolean',
             'min_purchase_price' => 'required_if:requires_min_purchase,true|exclude_unless:requires_min_purchase,true|numeric|gt:0',
             'for_first_purchase_only' => 'sometimes|boolean',
-            'max_usage' => 'sometimes|integer|gt:0',
-            'unlimted_usuage' => 'sometimes|boolean',
+            // 'max_usage' => 'sometimes|integer|gt:0',
+            // 'unlimited_usuage' => 'sometimes|boolean',
             'expiration_date' => 'required|date|after:'.now(),
             'store_id' => 'nullable|uuid|exists:stores,id',
 
